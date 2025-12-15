9a65b610bdb423064d1fc75c7d8d5219fc614b86
gthub-mohit <162956586+gthub-mohit@users.noreply.github.com>
2025-12-15 05:24:16 +0000

    question

---
+++ Daily Practice Problems/2.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int i , j , n ;
+    cin>>i;
+    for(j=0;j<i;j++){
+        cin>>n;
+        if(n==1)
+        cout<<200<<endl;
+        else
+        cout<<n*100<<endl;
+    }
+    return 0;
+}
